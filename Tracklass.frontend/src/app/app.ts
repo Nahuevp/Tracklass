8ba94693350a56b544cde2d97dc9ea6be31394ed
Nahuevp <nviera841@gmail.com>
2026-05-29 02:35:29 -0300

    feat: agregar ping asincrono a microservicios para reducir cold start

--- Tracklass.frontend/src/app/app.ts
+++ Tracklass.frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,13 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected title = 'estudiantes-frontend';
+
+  ngOnInit() {
+    // Despierta el backend de la app de Microservicios para reducir el cold start
+    // Fire and forget, no nos importa si falla o el resultado, solo queremos que levante.
+    fetch('https://ecommerce-microservices-ow4d.onrender.com/')
+      .catch(err => console.log('Ping a microservicios enviado (despertando backend)'));
+  }
 }
